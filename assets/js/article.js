fa99bb23855d2be2e6e61c001bbe33a3fb581990
sunnylost <sunnylost@gmail.com>
2014-03-04 09:09:35 +0800

    调整加载的 js 位置；去掉文件中的重复样式

--- assets/js/article.js
+++ assets/js/article.js
@@ -5,35 +5,10 @@
         win = window;
 
     !function() {
-        var progressTimeout,
-            resizeTimeout,
-            progressBar = doc.getElementsByClassName('progress')[0];
-
-        progress();
-
-        root.onscroll = progress;
-
-        win.onresize = function() {
-            clearTimeout(resizeTimeout);
-            resizeTimeout = setTimeout(function() {
-                progress();
-            }, 200);
-        };
-
         win.onload = function() {
             setTimeout(function() {
                 body.className = "";    
             }, 1000);
         };
-
-        /*
-            Progress Bar
-        */
-        function progress() {
-            clearTimeout(progressTimeout);
-            progressTimeout = setTimeout(function() {
-                progressBar.style.width = ((root.scrollTop + win.innerHeight) / root.scrollHeight) * 100 + '%';
-            }, 500);
-        }
     }();
 }()
